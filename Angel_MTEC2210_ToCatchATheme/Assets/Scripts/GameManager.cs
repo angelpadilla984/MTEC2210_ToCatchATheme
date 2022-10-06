05e24c5935248cb30e21d453e2bc661f06d4d8f5
Angel Padilla <angel.padilla@mail.citytech.cuny.edu>
2022-10-05 23:38:24 -0400

    Completed Week 4 (Hazard, Coin, and Score)

--- Angel_MTEC2210_ToCatchATheme/Assets/Scripts/GameManager.cs
+++ Angel_MTEC2210_ToCatchATheme/Assets/Scripts/GameManager.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
-
-    public GameObject itemPrefab;
+    public int score = 0;
+    public GameObject [] itemPrefab;
     public Transform leftTran;
     public Transform rightTran;
+
+    public TextMeshPro scoreText;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,17 +24,26 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        scoreText.text = "Score: " + score.ToString();
     }
 
     public void SpawnItem()
-    {
-        float rndXValue = Random.Range(leftTran.position.x, rightTran.position.x);
+        {
+            float rndXValue = Random.Range(leftTran.position.x, rightTran.position.x);
 
-        Vector2 spawnPos = new Vector2(rndXValue, leftTran.position.y);
+            Vector2 spawnPos = new Vector2(rndXValue, leftTran.position.y);
 
-        Instantiate(itemPrefab, spawnPos, Quaternion.identity);
+            int index = Random.Range(0, itemPrefab.Length); 
+            Instantiate(itemPrefab[index], spawnPos, Quaternion.identity);
+        }
+
+  
+
+        public void IncrementScore(int value)
+        {
+        score += value;
+        }
 
 
     }
-}
+
--- Angel_MTEC2210_ToCatchATheme/Assets/Scripts/PlayerControl.cs
+++ Angel_MTEC2210_ToCatchATheme/Assets/Scripts/PlayerControl.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
-    
+
 {
+
+    public GameManager gm;
     public float speed = 15;
 
     // Start is called before the first frame update
@@ -21,4 +23,30 @@ public class PlayerControl : MonoBehaviour
         //Debug.Log("xMove:" + xMove);
         transform.Translate(xMove * speed * Time.deltaTime, 0, 0);
     }
+
+    // private void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //  Debug.Log("Collided");
+
+    //}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Coin")
+        {
+            gm.IncrementScore(1);
+            Destroy(collision.gameObject);
+        }
+
+        if (collision.gameObject.tag == "Hazard")
+        {
+            Destroy(gameObject);
+        }
+        //Destroy(gameObject);
+        Debug.Log("Triggered");
+
+    }
 }
+
+
+    
